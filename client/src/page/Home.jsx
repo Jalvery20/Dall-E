75978ae4598d7c7d8f5aa44a69270e59fde3de9a
Jalvery20 <jesusalvarez010426@gmail.com>
2023-04-02 16:41:13 -0400

    Add files via upload

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dalleapi.onrender.com/api/v1/post', {
+      const response = await fetch('https://jalvery20imagenerator.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
